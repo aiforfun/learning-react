02526678bb5129b533e8690bc2b0c813edead33e
Thanh Nguyen Ky <thanh.nguyenky@singtel.com>
2018-01-13 15:05:03 +0800

    update structure /public

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 module.exports = {
   entry: [
-    './source/App.js'
+    __dirname + "/source/App.js"
   ],
   output: {
-    path: __dirname,
+    path: __dirname + "/public",
     filename: "bundle.js"
   }, module: {
     loaders: [{
       test: /\.jsx?$/,
       loader: 'babel'
     }]
+  },
+  devServer: {
+    contentBase: "./public",
+    colors: true,
+    historyApiFallback: true,
+    inline: true
   }
 };
